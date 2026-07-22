6b5cb3e4c7a882ff61a7cdc413a60548bcbef9e8
uysolanki <uysolanki@gmail.com>
2026-07-22 11:07:52 +0530

    initial commit

---
+++ src/main/java/com/itp/ITPJan2026Springboot/ItpJan2026SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.itp.ITPJan2026Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItpJan2026SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItpJan2026SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itp/ITPJan2026Springboot/ItpJan2026SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itp.ITPJan2026Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItpJan2026SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
