253ef4eec07a63f0b495497a75834cc339cdb746
uysolanki <uysolanki@gmail.com>
2026-07-29 10:02:44 +0530

    modified

--- src/main/java/com/itp/ITPJan2026Springboot/entity/Student.java
+++ src/main/java/com/itp/ITPJan2026Springboot/entity/Student.java
@@ -1,11 +1,14 @@
 package com.itp.ITPJan2026Springboot.entity;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,11 +25,31 @@ public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int rno;
+	
 	private String sname;
 	private double per;
 	private String course;
 	private String city;
 	private String email;
 	private LocalDate dob;
+	
+	private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+	
+	@PrePersist
+	protected void atCreation()
+	{
+		LocalDateTime now=LocalDateTime.now();
+		this.createdAt=now;
+		this.modifiedAt=now;
+	}
+	
+	@PreUpdate
+	protected void atUpdation()
+	{
+		this.modifiedAt=LocalDateTime.now();
+	}
+
+
 
 }
