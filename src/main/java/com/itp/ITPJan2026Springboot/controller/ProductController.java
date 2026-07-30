d7f141c743850f74b16a1b1035136c5eadd221e5
uysolanki <uysolanki@gmail.com>
2026-07-30 10:46:02 +0530

    Product controller added

--- src/main/java/com/itp/ITPJan2026Springboot/controller/ProductController.java
+++ src/main/java/com/itp/ITPJan2026Springboot/controller/ProductController.java
@@ -1,23 +1,35 @@
 package com.itp.ITPJan2026Springboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itp.ITPJan2026Springboot.dto.ProductDTO;
 import com.itp.ITPJan2026Springboot.service.ProductService;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 	
 	@Autowired
 	ProductService productService;
 	
+	@PostMapping("/saveProduct")
 	ResponseEntity<ProductDTO> saveProduct(@RequestBody ProductDTO productDto)
 	{
 		return  new ResponseEntity<ProductDTO>(productService.saveProduct(productDto),HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/saveProducts")
+	ResponseEntity<List<ProductDTO>> saveProducts(@RequestBody List<ProductDTO> productDtos)
+	{
+		return  new ResponseEntity<List<ProductDTO>>(productService.saveProducts(productDtos),HttpStatus.CREATED);
 	}
 
 }
--- src/main/java/com/itp/ITPJan2026Springboot/service/ProductService.java
+++ src/main/java/com/itp/ITPJan2026Springboot/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.itp.ITPJan2026Springboot.service;
 
+import java.util.List;
+
 import org.jspecify.annotations.Nullable;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,19 @@ public class ProductService {
 		Product product=modelMapper.map(productDto, Product.class);
 		Product productSavedInDB= productRepository.save(product);
 		return modelMapper.map(productSavedInDB,ProductDTO.class);
+	}
+
+	public @Nullable List<ProductDTO> saveProducts(List<ProductDTO> productDtos) {
+		
+		List<Product> products =productDtos.stream()
+		.map(dto->modelMapper.map(dto, Product.class))
+		.toList();
+		
+		List<Product> productsSavedInDB=productRepository.saveAll(products);
+		
+		return productsSavedInDB.stream()
+		.map(product->modelMapper.map(product, ProductDTO.class))
+		.toList();
 	}
 
 }
